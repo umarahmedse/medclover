a7768e47949c97dbb51ff41056c3dbf20b82ee89
Umar Ahmed <umarahmedse@outlook.com>
2025-01-25 01:53:52 +0500

    UI/UX Fixes

--- client/components/medical-case-status.tsx
+++ client/components/medical-case-status.tsx
@@ -15,7 +15,7 @@ export function MedicalCaseStatus({ isClosed, caseId }: MedicalCaseStatusProps) 
       variant={isClosed ? "default" : "secondary"}
       className={cn(
         "h-9 px-4 py-0 text-sm font-medium",
-        !isClosed ? "bg-green-500 hover:bg-green-600" : "bg-red-200 hover:bg-red-300",
+        !isClosed ? "bg-green-500 hover:bg-green-600" : "bg-red-600 hover:cursor-none hover:bg-red-600",
       )}
     >
       {!isClosed ? <LockOpen className="mr-2 h-4 w-4" /> : <Lock className="mr-2 h-4 w-4" />}
