b589d4d23bec285fb57d47c14e9563e0d004e8a8
Umar Ahmed <umarahmedse@outlook.com>
2025-01-24 08:39:05 +0500

    FIXED ROUTE

--- client/app/api/cases/[id]/route.ts
+++ client/app/api/cases/[id]/route.ts
@@ -2,9 +2,8 @@
 import { NextResponse } from "next/server";
 import connectToDB from "@/lib/mongodb";
 import { Case } from "@/models";
-
-export async function GET(request: Request, { params }: { params: { id: string } }) {
-  const { id } = params;
+export async function GET(request: Request, { params }: { params: Promise<{ id: string }> }) {
+  const { id } = await params;
 
   try {
     await connectToDB();
